16ea21bbe7b30cec305c52d36043f72a8c946f2b
Enthony Gabriel Bohm <114608439+EnthonyBohm@users.noreply.github.com>
2023-08-06 21:50:35 -0300

    Update trabalho2M1.c

--- Trabalho2/trabalho2M1.c
+++ Trabalho2/trabalho2M1.c
@@ -1,3 +1,8 @@
+/*
+Enthony Gabriel Rodrigues Bohm
+M1
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -13,6 +18,19 @@ int main () {
     result = fillCups(amount, 3);
     printf("%d", result);
 }
+
+/* 
+======================================================================
+fillCups
+
+    Dado um array com diferentes valores,
+    Onde cada valor representa a quantidade de copos a serem enchidos
+    Com diferentes tipos de água
+    Retornar o menor número de segundos onde é possível encher
+    2 copos de diferentes tipos de agua no mesmo segundo
+    1 copo de água do mesmo tipo em um segundo
+=====================================================================
+*/
 
 int fillCups (int* amount, int amountSize){
     
@@ -52,6 +70,16 @@ int fillCups (int* amount, int amountSize){
     }
     return seconds;
 }
+
+
+/* 
+======================================================================
+    empty
+
+Checa e retorna se o array esta vazio, ou seja, se todos os copos
+ja foram enchidos
+=====================================================================
+*/
 
 bool empty ( int* amount, int amountSize ) {
     for ( int i=0; i < amountSize; i++ ) {
